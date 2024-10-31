d3723faeb5ef8f1198ee785730977a3439acbf5e
RaihanDaffa04 <raihandaffaizzuddin77@gmail.com>
2024-10-31 18:51:03 +0800

    commit modifikasi

--- SearchNilai24.java
+++ SearchNilai24.java
@@ -1,18 +1,27 @@
+import java.util.Scanner;
+
 public class SearchNilai24 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
-
-        for (int i = 0; i < arrNilai.length; i ++) {
+        Scanner input = new Scanner(System.in);
+        int key, hasil = 0, jml;
+        System.out.print("Masukkan banyaknya nilai yang akan di input: ");
+        jml = input.nextInt();
+        int[] arrNilai = new int[jml];
+        for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke- " + (i+1) + ": ");
+            arrNilai[i] = input.nextInt();
+        }
+        System.out.print("Masukkan nilai mahasiswa yang ingin dicari: ");
+        key = input.nextInt();
+        for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
                 hasil = i;
+                System.out.println();
+                System.out.println("Nilai " + key + " Ketemu, merupakan nilai mahasiswa ke-" + (hasil+1));
+                System.out.println();
                 break;
-            }
+            } else System.out.println("Nilai yang dicari tidak ditemukan"); break;
         }
-        System.out.println();
-        System.out.println("Nilai "+key+" Ketemu di indeks ke-"+hasil);
-        System.out.println();
     }
-    
+
 }
