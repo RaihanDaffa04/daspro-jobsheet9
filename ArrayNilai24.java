43036ca69d1864b00cdcacb5394dbace97599d5d
RaihanDaffa04 <raihandaffaizzuddin77@gmail.com>
2024-10-31 17:34:41 +0800

    commit modifikasi

--- ArrayNilai24.java
+++ ArrayNilai24.java
@@ -4,13 +4,15 @@ public class ArrayNilai24 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
-        for (int i = 0; i < 10; i++) {
-            System.out.print("Masukkan nilai akhir ke-"+i+" : ");
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = input.nextInt();
-        } 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        }
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " lulus!");
+            } else System.out.println("Mahasiswa ke-" + i + " tidak lulus" );
         }
     }
-    
+
 }
